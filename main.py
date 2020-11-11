bd5992e4427791b2e0e0ea74c1e4c049cdbf0070
xana <xanarahmani@gmail.com>
2020-11-11 18:56:21 +0330

    Add prepare_text function

---
+++ main.py
@@ -0,0 +1,32 @@
+import csv
+import nltk
+
+
+def prepare_text(text, lang="en"):
+    if lang == "en":
+        tokens = nltk.word_tokenize(text)
+
+    return tokens
+
+
+def main():
+    with open('data/ted_talks.csv') as csv_file:
+        read_csv = csv.reader(csv_file)
+        fields = next(read_csv)
+        title_index = fields.index("title")
+        description_index = fields.index("description")
+        doc_id = 1
+        token = {}
+        for row in read_csv:
+            token[doc_id] = {
+                "title_token": prepare_text(text=row[title_index], lang="en"),
+                "description":  prepare_text(text=row[description_index], lang="en")
+            }
+            doc_id += 1
+
+        # Test
+        print(token[0])
+        print(token[1])
+
+
+main()
