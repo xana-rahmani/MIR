bae78abd364eaddeae3a0f7a17fe8ff346e79791
xana <xanarahmani@gmail.com>
2020-11-14 04:32:16 +0330

    Improve Coding

--- Second_part.py
+++ Second_part.py
@@ -1,21 +1,19 @@
 import json
-from main import read_file_and_create_token as rf_ct
+EN_Tokens = {}
+FA_Tokens = {}
+EN_Token_Repetition = {}
+FA_Token_Repetition = {}
+
+INVERTED_INDEX = {}
+Bigram_Index = {}
 
 
-def create_inverted_index(lang='en'):
-    """########################################
-
-                    Inverted Index
-
-    ########################################"""
-    index = {}
+def Create_Inverted_Index(lang='en', output_path='out.txt'):
     tokens_of_dictionary = []
     if lang == 'en':
-        tokens, _ = rf_ct(lang=lang)
-        output_path = 'english_index.txt'
+        tokens = EN_Tokens
     else:
-        tokens, _ = rf_ct(lang=lang)
-        output_path = 'persian_index.txt'
+        tokens = FA_Tokens
     for doc_id in tokens.keys():
         for token in set(tokens[doc_id]['title_token'] + tokens[doc_id]['description']):
             posting = {
@@ -45,35 +43,50 @@ def create_inverted_index(lang='en'):
             tokens_of_dictionary.append({'token': token, 'posting': posting, 'doc_id': doc_id})
             # the doc id is also in the 'posting'. but i also added it here to use it in the next line for sorting
     tokens_of_dictionary = sorted(tokens_of_dictionary, key=lambda k: (k['token'], k['doc_id']))
-    # merging the tokens to create an index
-    i = 0
-    while i < len(tokens_of_dictionary):
-        index[tokens_of_dictionary[i]['token']] = [tokens_of_dictionary[i]['posting']]
-        current_token = i
-        while i + 1 < len(tokens_of_dictionary) and tokens_of_dictionary[i + 1]['token'] == \
-                tokens_of_dictionary[current_token]['token']:
-            index[tokens_of_dictionary[current_token]['token']].append(
-                [tokens_of_dictionary[i + 1]['posting']])
+
+    Merge_Index_Posting(tokens_of_dictionary, load_file_path=output_path)
+    Write_And_Clear__Invert_Index(output_path)
+
+
+def Merge_Index_Posting(dic, load_file_path):
+    load_dic = Load__Invert_Index_File(load_file_path)
+    if load_dic is None:
+        i = 0
+        while i < len(dic):
+            INVERTED_INDEX[dic[i]['token']] = [dic[i]['posting']]
+            current_token = i
+            while i + 1 < len(dic) and dic[i + 1]['token'] == dic[current_token]['token']:
+                INVERTED_INDEX[dic[current_token]['token']].append([dic[i + 1]['posting']])
+                i += 1
             i += 1
-        i += 1
-
-    with open(output_path, 'w', encoding='utf-8') as f:
-        f.write(json.dumps(index, ensure_ascii=False))
-
-
-def create_bigrame(lang='en'):
-    """########################################
-
-                      Bigram
-
-    ########################################"""
-    if lang == 'en':
-        tokens, _ = rf_ct(lang=lang)
-        output_path = 'english_bigram.txt'
     else:
-        tokens, _ = rf_ct(lang=lang)
-        output_path = 'persian_bigram.txt'
-    bigram_index = {}
+        print("To DO")  # TODO
+
+
+def Write_And_Clear__Invert_Index(output_path):
+    with open(output_path, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(INVERTED_INDEX, ensure_ascii=False))
+    INVERTED_INDEX.clear()
+
+
+def Load__Invert_Index_File(load_file_path):
+    try:
+        with open(load_file_path, 'r') as file:
+            data = file.read()
+            if len(data) == 0:
+                return None
+            index = json.loads(data)
+            return index
+    except Exception as e:
+        print(10)
+        return None
+
+
+def Create_Bigrame(lang='en', output_path='out.txt'):
+    if lang == 'en':
+        tokens = EN_Tokens
+    else:
+        tokens = FA_Tokens
     all_tokens = set()
     for key in tokens.keys():
         for token in set(tokens[key]['title_token'] + tokens[key]['description']):
@@ -84,14 +97,23 @@ def create_bigrame(lang='en'):
         t = "$" + token + "$"
         for i in range(0, len(t) - 1):
             bigrame = t[i:i+2]
-            if bigrame in bigram_index.keys():
-                bigram_index[bigrame].append(token)
+            if bigrame in Bigram_Index.keys():
+                Bigram_Index[bigrame].append(token)
             else:
-                bigram_index[bigrame] = [token]
-    import json
+                Bigram_Index[bigrame] = [token]
+
+    Write_And_Clear__Bigram_Index(output_path)
+
+
+def Write_And_Clear__Bigram_Index(output_path):
     with open(output_path, 'w', encoding='utf-8') as f:
-        f.write(json.dumps(bigram_index))
+        f.write(json.dumps(Bigram_Index, ensure_ascii=False))
+    Bigram_Index.clear()
 
 
-create_inverted_index("fa")
-# create_bigrame('en')
+def AddDoc():
+    pass
+
+
+def RemoveDoc():
+    pass
